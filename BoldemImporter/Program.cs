f244a828c0e79daf3d82a93e3eab1b80c353128f
Dr0pyyyy <adamkoutny12345@seznam.cz>
2023-08-12 10:13:23 +0200

    Načtení dat z xml souboru + vytvoření User class

--- BoldemImporter/Program.cs
+++ BoldemImporter/Program.cs
@@ -1,1 +1,28 @@
-﻿
+﻿using BoldemImporter;
+using System;
+using System.Xml;
+
+namespace XMLLoadingExample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string xmlFilePath = @"C:\Users\adamk\OneDrive\Plocha\Programming\C#\BoldemImporter\userData.xml";
+
+			XmlDocument xmlDoc = new XmlDocument();
+			xmlDoc.Load(xmlFilePath);
+			XmlNodeList userList = xmlDoc.GetElementsByTagName("user");
+			List<User> users = new List<User>();
+
+			foreach (XmlNode userNode in userList)
+			{
+				User user = new User();
+				user.Firstname = userNode.SelectSingleNode("firstname").InnerText;
+				user.Lastname = userNode.SelectSingleNode("lastname").InnerText;
+				user.Email = userNode.SelectSingleNode("email").InnerText;
+				users.Add(user);
+			}
+		}
+	}
+}
